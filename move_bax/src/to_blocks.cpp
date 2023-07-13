6c1e3084ec0ca72aa4e42c2f0451abfc298f8534
Giordano <arcierigiordano@gmail.com>
2023-07-13 16:16:14 -0400

    going on top of blocks

--- move_bax/src/to_blocks.cpp
+++ move_bax/src/to_blocks.cpp
@@ -4,10 +4,12 @@
 #include <geometry_msgs/Pose.h>
 #include <queue>
 
+geometry_msgs::Pose createPose(double x, double y, double z);
+
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "say_hello");
+    ros::init(argc, argv, "go_to_blocks");
     ros::NodeHandle nh;
     ros::AsyncSpinner spinner(1);
     spinner.start();
@@ -22,14 +24,62 @@ int main(int argc, char** argv)
     move_group.setMaxAccelerationScalingFactor(0.2);   
 
     // Sleeping object 
-    ros::Duration aspe(5);
+    ros::Duration tfstall(5);
+    ros::Duration aspe(7);
+
+    // Define target poses queue
+    std::queue<geometry_msgs::Pose> target_poses;
+    double treshold = 0.25;
+
+    double x = 0.74, y = -0.31, z = -0.16;
 
 
-    
+    // Create target poses and add them to the queue
+    target_poses.push(createPose(x, y, z + treshold));
+
+    // Execute poses from the queue
+    while (!target_poses.empty())
+    {
+        // Get the next pose from the queue
+        geometry_msgs::Pose target_pose = target_poses.front();
+        target_poses.pop();
+
+        // Plan and execute the motion
+        move_group.setPoseTarget(target_pose);
+
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+        bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+        if (success)
+        {
+            ROS_INFO("Planning successful. Moving the robot...");
+            move_group.execute(my_plan);
+            ROS_INFO("Movement Succesful.");
+        }
+        else
+        {
+            ROS_ERROR("Planning failed!");
+            return 1;
+        }
+    }
+
     spinner.stop();
     ros::shutdown();
 
     return 0;
 }
 
+// Function to create a target pose based on provided parameters
+geometry_msgs::Pose createPose(double x, double y, double z)
+{
+  geometry_msgs::Pose pose;
+  pose.position.x = x;
+  pose.position.y = y;
+  pose.position.z = z;
+  pose.orientation.x = 0;
+  pose.orientation.y = 1;
+  pose.orientation.z = 0;
+  pose.orientation.w = 0;
+  return pose;
+}
 
